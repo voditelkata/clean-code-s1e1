1132f6fae01a339c06b201d770e2c6e565b1f7e8
voditelkatka <voditelkatka@gmail.com>
2022-05-30 18:16:19 +0300

    refactor: add laconic names for classes/id  according rule 3.3 from html-and-css

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ var createNewTaskElement = function (taskString) {
   editInput.className = "task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "edit-task";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "delete-task";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -76,7 +76,7 @@ var editTask = function () {
 
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
+  var editBtn = listItem.querySelector(".edit-task");
   var containsClass = listItem.classList.contains("edit-mode");
   //If class of the parent is .editmode
   if (containsClass) {
@@ -138,8 +138,8 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var editButton = taskListItem.querySelector("button.edit-task");
+  var deleteButton = taskListItem.querySelector("button.delete-task");
 
   //Bind editTask to edit button.
   editButton.onclick = editTask;
